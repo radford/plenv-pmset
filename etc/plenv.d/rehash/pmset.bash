3d26d56ed9bf997d518bc671d4eac69a72f74e21
Jim Radford <radford@blackbean.org>
2013-06-24 15:24:36 -0700

    Simplify rehash
    Fix quoting.  No need to cd or set nullglob as they are done in plenv-rehash.

--- etc/plenv.d/rehash/pmset.bash
+++ etc/plenv.d/rehash/pmset.bash
@@ -1,10 +1,1 @@
-shopt -s nullglob
-OLDIFS="$IFS"
-IFS=$' \t\n'
-pmset_bin=(${PLENV_ROOT}/versions/*/pmsets/*/bin/*)
-IFS="$OLDIFS"
-shopt -s nullglob
-
-cd "$SHIM_PATH"
-make_shims ${pmset_bin[@]}
-cd "$CUR_PATH"
+make_shims ${PLENV_ROOT}/versions/*/pmsets/*/bin/*
